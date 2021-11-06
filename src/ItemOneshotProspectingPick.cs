c9ea45d6ac38d03f8a29195febf9223f017e842f
spoon <ano@example.com>
2021-11-06 23:18:30 +0900

    First commit

---
+++ src/ItemOneshotProspectingPick.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.API;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+using Vintagestory.ServerMods;
+using Vintagestory.GameContent;
+
+namespace oneshotpropick
+{
+    public class ItemOneshotProspectingPick : ItemProspectingPick
+    {
+        protected override void ProbeBlockDensityMode(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel)
+        {
+            /*            base.ProbeBlockDensityMode(world, byEntity, itemslot, blockSel);*/
+            IPlayer byPlayer = null;
+            if (byEntity is EntityPlayer) byPlayer = world.PlayerByUid(((EntityPlayer)byEntity).PlayerUID);
+
+            Block block = world.BlockAccessor.GetBlock(blockSel.Position);
+            float dropMul = 1f;
+            if (block.BlockMaterial == EnumBlockMaterial.Ore || block.BlockMaterial == EnumBlockMaterial.Stone) dropMul = 0;
+
+            block.OnBlockBroken(world, blockSel.Position, byPlayer, dropMul);
+
+            if (!block.Code.Path.StartsWith("rock") && !block.Code.Path.StartsWith("ore")) return;
+
+            IServerPlayer splr = byPlayer as IServerPlayer;
+
+            if (splr == null) return;
+
+            PrintProbeResults(world, splr, itemslot, blockSel.Position);
+            return;
+        }
+    }
+}
