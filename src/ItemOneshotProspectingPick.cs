37ef9c426178a7d53ce5b1c585d2bbab3b62efb5
spoon <ano@example.com>
2021-11-07 02:44:25 +0900

    WIP

--- src/ItemOneshotProspectingPick.cs
+++ src/ItemOneshotProspectingPick.cs
@@ -5,27 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
-using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
-using Vintagestory.API;
-using Vintagestory.API.Client;
-using Vintagestory.API.Common;
-using Vintagestory.API.Common.Entities;
-using Vintagestory.API.Config;
-using Vintagestory.API.Datastructures;
-using Vintagestory.API.MathTools;
-using Vintagestory.API.Server;
-using Vintagestory.API.Util;
-using Vintagestory.ServerMods;
 using Vintagestory.GameContent;
+using System.Reflection;
 
 namespace oneshotpropick
 {
     public class ItemOneshotProspectingPick : ItemProspectingPick
     {
-        protected override void ProbeBlockDensityMode(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel)
+        public ItemOneshotProspectingPick():base()
         {
-            /*            base.ProbeBlockDensityMode(world, byEntity, itemslot, blockSel);*/
+            System.Diagnostics.Debug.WriteLine("Created!");
+
+        }
+
+        protected override void ProbeBlockDensityMode(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel) 
+        {
+            System.Diagnostics.Debug.WriteLine($"Invoked Side:{world.Side}");
+
+            ProbeBlockDensityModeInternal(world, byEntity, itemslot, blockSel);
+
+            Assembly[] asms = AppDomain.CurrentDomain.GetAssemblies();
+            /*            foreach (Assembly a in asms)
+                            System.Diagnostics.Debug.WriteLine(a.FullName);
+            */
+            var assem = asms.SingleOrDefault(assembly => assembly.GetName().Name == "ProspectorInfo");
+            //Type type = Type.GetType("ProspectorInfo.Map.ProspectorOverlayLayer, ProspectorInfo");
+            //System.Diagnostics.Debug.WriteLine($"OverlayLayer:{type == null}");
+            //var type = assem.GetType("ProspectorInfo.Map.ProspectorOverlayLayer+PrintProbeResultsPatch");
+            var type = Type.GetType("ProspectorInfo.Map.ProspectorOverlayLayer+PrintProbeResultsPatch,ProspectorInfo");
+            //var type = assem.GetType("ProspectorInfo.Map.ProspectorOverlayLayer");
+            MethodInfo meth = type.GetMethod("Postfix", BindingFlags.Static | BindingFlags.NonPublic);
+            //FieldInfo fi = type.GetField("blocksSinceLastSuccessList", BindingFlags.Static | BindingFlags.NonPublic);
+            //var val = fi.GetValue(null) as List<BlockSelection>;
+
+            System.Diagnostics.Debug.WriteLine($"{world.Side}");
+            //System.Diagnostics.Debug.WriteLine($"pros:{val.Count}");
+            System.Diagnostics.Debug.WriteLine($"Hoge");
+            /*            val.Add(blockSel);
+                        val.Add(blockSel);
+                        val.Add(blockSel);
+            */
+            meth.Invoke(null, new object[] { world, byEntity, itemslot, blockSel });
+            meth.Invoke(null, new object[] { world, byEntity, itemslot, blockSel });
+            meth.Invoke(null, new object[] { world, byEntity, itemslot, blockSel });
+
+        }
+
+        protected virtual void ProbeBlockDensityModeInternal(IWorldAccessor world, Entity byEntity, ItemSlot itemslot, BlockSelection blockSel)
+        {
+
             IPlayer byPlayer = null;
             if (byEntity is EntityPlayer) byPlayer = world.PlayerByUid(((EntityPlayer)byEntity).PlayerUID);
 
@@ -42,6 +71,7 @@ namespace oneshotpropick
             if (splr == null) return;
 
             PrintProbeResults(world, splr, itemslot, blockSel.Position);
+
             return;
         }
     }
--- src/OneshotPropickMod.cs
+++ src/OneshotPropickMod.cs
@@ -4,19 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Common;
-using Vintagestory.API.Common.Entities;
-using Vintagestory.API.MathTools;
-using Vintagestory.API.Server;
-using Vintagestory.API;
-using Vintagestory.API.Client;
-using Vintagestory.API.Common;
-using Vintagestory.API.Common.Entities;
-using Vintagestory.API.Config;
-using Vintagestory.API.Datastructures;
-using Vintagestory.API.MathTools;
-using Vintagestory.API.Server;
-using Vintagestory.API.Util;
-using Vintagestory.ServerMods;
 
 namespace oneshotpropick
 {
